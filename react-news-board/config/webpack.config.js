7eea04fc14d2592e4631a8827eabbf3f6f7633de
Dzmitry Hrachou <falizyer@gmail.com>
2020-02-22 04:39:57 +0300

    refactor build system

---
+++ react-news-board/config/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const webpack = require("webpack");
+const yaml = require("yaml");
+const fs = require("graceful-fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const rootPath = path.resolve(__dirname, "../");
+
+console.log(yaml.parse(require("./development.yaml")));
+
+const configFile = fs.readFileSync(`./${yargs.argv.mode}.yaml`);
+const config = yaml.parse(configFile);
+
+module.exports = {
+    target: 'web',
+
+    node: { fs: 'empty' },
+
+    context: path.resolve(__dirname, "../"),
+
+    mode: yargs.argv.mode,
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    entry: "./index.tsx",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".scss"],
+        modules: ["node_modules"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin(config.env),
+        new HtmlWebpackPlugin({
+            template: path.resolve(rootPath, "./public/index.ejs"),
+            meta: config.meta
+        })
+    ],
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    // TODO check that webpack is waiting for external modules if they are not bundled with webpack
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
+};
